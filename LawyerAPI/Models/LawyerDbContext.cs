773e08d6d4217a41d657d55a414aa56f6e16ced9
mohitt2d <104139311+mohitt2d@users.noreply.github.com>
2022-10-27 14:27:50 +0000

    login, register added

--- LawyerAPI/Models/LawyerDbContext.cs
+++ LawyerAPI/Models/LawyerDbContext.cs
@@ -11,14 +11,24 @@ namespace LawyerAPI.Models
         }
 
         public DbSet<Court> Courts { get; set; } = null!;
+        public DbSet<User> Users { get; set; } = null!;
         public DbSet<Lawyer> Lawyers { get; set; } = null!;
         public DbSet<CourtCaseAgenda> CourtCaseAgenda { get; set; } = null!;
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Entity<User>().Property(f => f.ID).ValueGeneratedOnAdd();            
             modelBuilder.Entity<Court>().ToTable("Courts");
-            modelBuilder.Entity<Lawyer>().ToTable("Lawyers");
             modelBuilder.Entity<CourtCaseAgenda>().ToTable("CourtCaseAgendas");
+            modelBuilder.Entity<Lawyer>().ToTable("Lawyers");
+            /*
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(e => e.ID);
+            });
+            */
         }
 
     }
